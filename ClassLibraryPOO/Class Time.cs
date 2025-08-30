4ba3d2e6fb4cdd72055dfd2aa6b8a8f1f8670fba
Carlo5K4b <minimalkab@gmail.com>
2025-08-30 16:20:45 -0500

    Builder Time

---
+++ ClassLibraryPOO/Class Time.cs
@@ -0,0 +1,86 @@
+﻿namespace ClassLibraryPOO
+{
+    // Taller#1 POO
+    using System;
+    using System.ComponentModel;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class Time
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+        private int milliseconds;
+
+        //Constructor (#1) sin parámetros
+        public Time(int hours, int minutes, int seconds, int milliseconds)
+        {
+
+            if (!IsValidTime(hours, minutes, seconds, milliseconds))
+            {
+                throw new ArgumentException($"The hour: {hours}, is not valid.");
+            }
+
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.milliseconds = milliseconds;
+        }
+
+        // Método validar (si) la hora es válida
+        private bool IsValidTime(int hours, int minutes, int seconds, int milliseconds)
+        {
+            return hours >= 0 && hours < 24 &&
+                   minutes >= 0 && minutes < 60 &&
+                   seconds >= 0 && seconds < 60 &&
+                   milliseconds >= 0 && milliseconds < 1000;
+        }
+
+        // Método ToMilliseconds
+        public long ToMilliseconds()
+        {
+            return (long)hours * 3600000 + minutes * 60000 + seconds * 1000 + milliseconds;
+        }
+
+        // Método ToSeconds|
+        public long ToSeconds()
+        {
+            return ToMilliseconds() / 1000;
+        }
+
+        // Método ToMinutes
+        public long ToMinutes()
+        {
+            return ToMilliseconds() / 60000;
+        }
+
+        // Método Add
+        public Time Add(Time other, out bool isOtherDay)
+        {
+            long totalMs = this.ToMilliseconds() + other.ToMilliseconds();
+
+            // Un día en milisegundos
+            long msInDay = 24 * 3600000;
+
+            isOtherDay = totalMs >= msInDay;
+            totalMs %= msInDay;
+
+            int newHours = (int)(totalMs / 3600000);
+            totalMs %= 3600000;
+
+            int newMinutes = (int)(totalMs / 60000);
+            totalMs %= 60000;
+
+            int newSeconds = (int)(totalMs / 1000);
+            int newMilliseconds = (int)(totalMs % 1000);
+
+            return new Time(newHours, newMinutes, newSeconds, newMilliseconds);
+        }
+
+        // Método para mostrar la hora en formato "HH:MM:SS.mmm"
+        public override string ToString()
+        {
+            return $"{hours:D2}:{minutes:D2}:{seconds:D2}.{milliseconds:D3}";
+        }
+    }
+}
---
+++ Console_TallerPOO/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using ClassLibraryPOO;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            var t1 = new Time(0, 0, 0, 0);
+            Imprimir(t1, new Time(9, 34, 0, 0));
+
+            var t2 = new Time(2, 0, 0, 0);
+            Imprimir(t2, new Time(9, 34, 0, 0));
+
+            var t3 = new Time(9, 34, 0, 0);
+            Imprimir(t3, new Time(9, 34, 0, 0));
+
+            var t4 = new Time(7, 45, 56, 0);
+            Imprimir(t4, new Time(9, 34, 0, 0));
+
+            var t5 = new Time(11, 3, 45, 678);
+            Imprimir(t5, new Time(9, 34, 0, 0));
+
+            // Este dará error
+            var t6 = new Time(45, 0, 0, 0);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static void Imprimir(Time t, Time add)
+    {
+        Console.WriteLine($"Time: {t}");
+        Console.WriteLine($" Milliseconds : {t.ToMilliseconds():N0}");
+        Console.WriteLine($" Seconds      : {t.ToSeconds():N0}");
+        Console.WriteLine($" Minutes      : {t.ToMinutes():N0}");
+
+        bool isOtherDay;
+        var result = t.Add(add, out isOtherDay);
+        Console.WriteLine($" Add          : {result}");
+        Console.WriteLine($" Is Other day : {isOtherDay}");
+        Console.WriteLine();
+    }
+}
